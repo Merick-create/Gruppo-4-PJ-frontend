f0f21b01aae85393ea57ac43f421b59948856ed7
Turcheezz <andreaturchish@gmail.com>
2025-09-18 15:59:48 +0200

    app-routing fix

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './components/login/login.component';
-import { RegisterComponent } from './components/register/register.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { HomeComponent } from './pages/homepage/homepage.component';
 
 
 const routes: Routes = [
@@ -12,6 +13,10 @@ const routes: Routes = [
   {
     path:'',
     component:RegisterComponent
+  },
+  {
+    path:'home',
+    component: HomeComponent
   }
 ];
 
