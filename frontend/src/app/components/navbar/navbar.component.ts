b3b5e6ee9561b0fe2772f877813cf2b149d674c6
Merick-create <ric05bras@gmail.com>
2025-09-20 10:52:15 +0200

    pulsante logout

--- frontend/src/app/components/navbar/navbar.component.ts
+++ frontend/src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject} from '@angular/core';
 import { AuthService } from '../../services/auth.service';
 import { Router } from '@angular/router';
 
@@ -13,9 +13,25 @@ export class NavbarComponent {
   protected router=inject(Router);
 
   currentUser$ = this.authSrv.currentUser$;
+  menuOpen: boolean = false;
+
 
   logout() {
     this.authSrv.logout();
     this.router.navigate(['/login']);
+    this.menuOpen = false;
+  }
+
+  
+  toggleMenu(): void {
+    this.menuOpen = !this.menuOpen;
+  }
+
+  showMenu(): void {
+    this.menuOpen = true;
+  }
+
+  hideMenu(): void {
+    this.menuOpen = false;
   }
 }
