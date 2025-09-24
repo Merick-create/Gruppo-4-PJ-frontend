f5b1ac9f999e082d03b60e4db57419bbed55761f
Lorenzo <lorenzo.forner@allievi.itsdigitalacademy.com>
2025-09-24 14:27:31 +0200

    Fix categoria nome

--- frontend/src/app/entities/Movimento.entity.ts
+++ frontend/src/app/entities/Movimento.entity.ts
@@ -1,9 +1,10 @@
+import { Categoria } from "./categorie";
+
 export interface Movimento {
   ContoCorrenteId: string;
   dataCreazione?: string; // o Date, se vuoi gestire direttamente Date
   importo: number;
   saldo?: number;
-  CategoriaMovimentoid?: string; // rimane l’ID se serve
-  NomeCategoria?: string;       // campo aggiunto dal backend
+  CategoriaMovimentoid?: Categoria; // rimane l’ID se serve
   descrizione: string;
 }
