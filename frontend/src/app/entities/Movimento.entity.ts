13d0d96fff397f63357b915bdddb19b9768ca1fe
Merick-create <ric05bras@gmail.com>
2025-09-25 11:45:27 +0200

    sus2

--- frontend/src/app/entities/Movimento.entity.ts
+++ frontend/src/app/entities/Movimento.entity.ts
@@ -1,8 +1,9 @@
 import { Categoria } from "./categorie";
+import { User } from "./user.entity";
 
 export interface Movimento {
   _id?: string;
-  ContoCorrenteId: string;
+  ContoCorrenteId: User;
   dataCreazione?: string; 
   importo: number;
   saldo?: number;
